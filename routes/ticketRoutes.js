50a1b596f7dca3f53ea814afeb14970d4dc8744e
Daniel <adjeijuniordaniel@gmail.com>
2023-12-05 08:58:39 +0000

    created the delete controller

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { getAllTickets, createNewTicket, findSingleTicket } from '../controllers/ticketController.js'
+import { getAllTickets, createNewTicket, findSingleTicket, deleteTicket } from '../controllers/ticketController.js'
 
 export const router = Router()
 
 router.get('/', getAllTickets)
 router.post('/create', createNewTicket)
 router.get('/:id', findSingleTicket )
+router.delete('/delete/:id', deleteTicket)
 
 
 export const ticketRoutes = router
